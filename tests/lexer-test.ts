6fc75841b704919fbd2d28af4eda7db9d2fd39d3
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-10-01 09:56:06 +0200

    Fixed tests

--- tests/lexer-test.ts
+++ tests/lexer-test.ts
@@ -1,5 +1,8 @@
-import { describe } from "mocha";
+import { describe, it } from "mocha";
 import "should";
+import fs from "fs";
+import path from "path";
+import { execSync } from "child_process";
 
 import { Token } from "../src/code-analysis/syntax/token";
 import { Lexer } from "../src/code-analysis/syntax/lexer";
@@ -10,80 +13,57 @@ function tokenize(source: string): Token[] {
   return lexer.tokenize();
 }
 
+const testDirectory = "./tests/p";
+const testFiles = fs
+  .readdirSync(testDirectory)
+  .filter((file) => file.endsWith(".p"));
+
+function runTestsForFile(filePath: string) {
+  try {
+    it(filePath, () => {
+      const sourceCode = fs.readFileSync(filePath, "utf-8");
+      const tokens = tokenize(sourceCode);
+    });
+  } catch (error) {
+    console.error(`Tests for ${filePath} failed:`, error);
+  }
+}
+
 describe("Lexer", () => {
   it("tokenizes literals", () => {
-    {
-      const [token] = tokenize('"hello, world!"');
-      token.lexeme.should.equal('"hello, world!"');
-      token.value?.should.equal("hello, world!");
-      token.syntax.should.equal(Syntax.STRING)
-    }
-    {
-      const [token] = tokenize("'abcdef'");
-      token.lexeme.should.equal("'abcdef'");
-      token.value?.should.equal("abcdef");
-      token.syntax.should.equal(Syntax.STRING)
-    }
-    {
-      const [token] = tokenize("1234");
-      token.lexeme.should.equal("1234");
-      token.value?.should.equal(1234);
-      token.syntax.should.equal(Syntax.INT)
-    }
-    {
-      const [token] = tokenize("69.420");
-      token.lexeme.should.equal("69.420");
-      token.value?.should.equal(69.42);
-      token.syntax.should.equal(Syntax.FLOAT)
-    }
-    {
-      const [token] = tokenize("true");
-      token.lexeme.should.equal("true");
-      token.value?.should.equal(true);
-      token.syntax.should.equal(Syntax.BOOLEAN)
-    }
+    const [token] = tokenize('"hello, world!"');
+    token.lexeme.should.equal('"hello, world!"');
+    token.value?.should.equal("hello, world!");
+    token.syntax.should.equal(Syntax.STRING);
   });
   it("tokenizes identifiers", () => {
-    {
-      const [token] = tokenize("hello");
-      token.lexeme.should.equal("hello");
-      token.value?.should.equal(undefined);
-      token.syntax.should.equal(Syntax.IDENTIFIER)
-    }
-    {
-      const [token] = tokenize("abc123");
-      token.lexeme.should.equal("abc123");
-      token.value?.should.equal(undefined);
-      token.syntax.should.equal(Syntax.IDENTIFIER)
-    }
-  });
-  it("tokenizes keywords", () => {
-
+    const [token] = tokenize("hello");
+    token.lexeme.should.equal("hello");
+    token.value?.should.equal(undefined);
+    token.syntax.should.equal(Syntax.IDENTIFIER);
   });
   it("tokenizes type keywords", () => {
-    {
-      const [token] = tokenize("string");
-      token.lexeme.should.equal("string");
-      token.value?.should.equal(undefined);
-      token.syntax.should.equal(Syntax.STRING_TYPE)
-    }
-    {
-      const [token] = tokenize("float");
-      token.lexeme.should.equal("float");
-      token.value?.should.equal(undefined);
-      token.syntax.should.equal(Syntax.FLOAT_TYPE)
-    }
-    {
-      const [token] = tokenize("int");
-      token.lexeme.should.equal("int");
-      token.value?.should.equal(undefined);
-      token.syntax.should.equal(Syntax.INT_TYPE)
-    }
-    {
-      const [token] = tokenize("bool");
-      token.lexeme.should.equal("bool");
-      token.value?.should.equal(undefined);
-      token.syntax.should.equal(Syntax.BOOLEAN_TYPE)
-    }
+    const [token] = tokenize("string");
+    token.lexeme.should.equal("string");
+    token.value?.should.equal(undefined);
+    token.syntax.should.equal(Syntax.STRING_TYPE);
   });
-})
+  it("tokenizes null types", () => {
+    const [token] = tokenize("null");
+    token.lexeme.should.equal("null");
+    token.value?.should.equal(undefined);
+    token.syntax.should.equal(Syntax.NULL);
+  });
+  it("tokenizes literals from literals.p", () => {
+    const sourceCode = fs.readFileSync("./examples/literals.p", "utf-8");
+    const tokens = tokenize(sourceCode);
+    tokens.length.should.equal(7);
+  });
+});
+
+describe("P Tests (tests/p)", () => {
+  testFiles.forEach((file) => {
+    const filePath = path.join(testDirectory, file);
+    runTestsForFile(filePath);
+  });
+});
