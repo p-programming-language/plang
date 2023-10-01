042286f4f2f84f386c91c5084da661647baa67f3
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-10-01 11:42:53 +0200

    Added VariableTypes to prepare for variable parsing

---
+++ src/code-analysis/syntax/variable-type.ts
@@ -0,0 +1,17 @@
+import Syntax from "./syntax-type";
+
+
+export const VariableTypes = [
+    Syntax.INT,
+    Syntax.FLOAT,
+    Syntax.STRING,
+    Syntax.BOOLEAN,
+    Syntax.FUNCTION,
+    Syntax.INT_TYPE,
+    Syntax.FLOAT_TYPE,
+    Syntax.STRING_TYPE,
+    Syntax.BOOLEAN_TYPE,
+    Syntax.VOID_TYPE,
+    Syntax.NULL,
+    Syntax.UNDEFINED
+]
