a16b6962caafedcb33431a4f30413a1ea3bfb255
R-unic <roleyspersonal@gmail.com>
2023-10-09 15:42:28 -0400

    feat: add all output flags to pint CLI

--- tools/cli/pint.ts
+++ tools/cli/pint.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-import { command, option, optional, restPositionals, string, run } from "cmd-ts"
+import { command, option, flag, optional, restPositionals, string, run } from "cmd-ts"
 import P from "../p";
 import pkg = require("../../package.json");
+import { PError } from "../../src/errors";
 
 const pint = command({
   name: "pint",
@@ -13,20 +14,43 @@ const pint = command({
       short: "f",
       description: "Path to the file to execute P on"
     }),
+    tokens: flag({
+      long: "tokens",
+      description: "Print tokens output"
+    }),
+    ast: flag({
+      long: "ast",
+      description: "Print AST output"
+    }),
+    boundAST: flag({
+      long: "bound-ast",
+      description: "Print bound AST output"
+    }),
+    results: flag({
+      long: "results",
+      description: "Print evaluation result output"
+    }),
+    trace: flag({
+      long: "trace",
+      short: "t",
+      description: "Print full PError traces"
+    }),
     args: restPositionals({
       type: string,
       displayName: "args",
       description: "The arguments to pass onto P"
     })
   },
-  handler({ filePath, args }): void {
+  handler({ filePath, args, tokens, ast, boundAST, results, trace }): void {
     const p = new P(filePath ?? "repl");
     if (!filePath || filePath !== "string")
       return p.repl.start(args);
 
-    p.executionOptions.outputAST = false;
-    p.executionOptions.outputBoundAST = false;
-    p.executionOptions.outputResult = false;
+    p.executionOptions.outputTokens = tokens;
+    p.executionOptions.outputAST = ast;
+    p.executionOptions.outputBoundAST = boundAST;
+    p.executionOptions.outputResult = results;
+    PError.showTrace = trace;
     p.doFile(filePath, args);
   }
 })
