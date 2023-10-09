3be417485d563fbc227359cbd13b9ea6ca4af982
R-unic <roleyspersonal@gmail.com>
2023-10-09 16:14:28 -0400

    fix: accepting pint CLI arguments

--- tools/cli/pint.ts
+++ tools/cli/pint.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { command, option, flag, optional, restPositionals, string, run } from "cmd-ts"
+import { command, positional, flag, optional, restPositionals, string, run } from "cmd-ts"
 import P from "../p";
 import pkg = require("../../package.json");
 import { PError } from "../../src/errors";
@@ -8,12 +8,6 @@ const pint = command({
   name: "pint",
   version: "v" + pkg.version,
   args: {
-    filePath: option({
-      type: optional(string),
-      long: "file",
-      short: "f",
-      description: "Path to the file to execute P on"
-    }),
     tokens: flag({
       long: "tokens",
       description: "Print tokens output"
@@ -35,21 +29,27 @@ const pint = command({
       short: "t",
       description: "Print full PError traces"
     }),
+    filePath: positional({
+      type: optional(string),
+      displayName: "file path",
+      description: "Path to the file to execute P on"
+    }),
     args: restPositionals({
       type: string,
       displayName: "args",
       description: "The arguments to pass onto P"
     })
   },
-  handler({ filePath, args, tokens, ast, boundAST, results, trace }): void {
+  handler({ tokens, ast, boundAST, results, trace, filePath, args }): void {
     const p = new P(filePath ?? "repl");
-    if (!filePath || filePath !== "string")
-      return p.repl.start(args);
-
     p.executionOptions.outputTokens = tokens;
     p.executionOptions.outputAST = ast;
     p.executionOptions.outputBoundAST = boundAST;
     p.executionOptions.outputResult = results;
+
+    if (!filePath || !filePath.endsWith(".p") || !filePath.includes("/"))
+      return p.repl.start(filePath ? [filePath].concat(...args) : args);
+
     PError.showTrace = trace;
     p.doFile(filePath, args);
   }
