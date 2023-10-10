3f3d5979d73576b6a7d7a95aa3efcf9a6bab97d6
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-10-10 16:25:34 +0200

    feat: Added exec to @std/system

--- src/runtime/intrinsics/libs/std/system.ts
+++ src/runtime/intrinsics/libs/std/system.ts
@@ -1,10 +1,13 @@
 import { platform } from "os";
+import toCamelCase from "to-camel-case";
+import { execSync } from 'child_process';
 
 import type { ValueType } from "../../../../code-analysis/type-checker";
 import type { Type } from "../../../../code-analysis/type-checker/types/type";
 import UnionType from "../../../../code-analysis/type-checker/types/union-type";
 import Intrinsic from "../../../values/intrinsic";
 import LiteralType from "../../../../code-analysis/type-checker/types/literal-type";
+import SingularType from "../../../../code-analysis/type-checker/types/singular-type";
 
 export default class SystemLib extends Intrinsic.Lib {
   public get propertyTypes(): Record<string, Type> {
@@ -28,7 +31,22 @@ export default class SystemLib extends Intrinsic.Lib {
   public get members(): Record<string, ValueType> {
     const libName = this.name;
     return {
-      os: platform()
+      os: platform(),
+
+      exec: class Exec extends Intrinsic.Function {
+        public readonly name = `${libName}.${toCamelCase(this.constructor.name)}`;
+        public readonly returnType = new SingularType("string");
+        public readonly argumentTypes = { message: new SingularType("string") };
+
+        public call(command: string): string {
+          try {
+            const output = execSync(command, { encoding: 'utf-8' });
+            return output;
+          } catch (error: any) {
+            return error;
+          }
+        }
+      },
     };
   }
 }
