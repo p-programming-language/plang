523a8486438d937dc457456e283fc0b5e4d72ed4
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-10-10 16:04:19 +0200

    fix: Made opening a file work in pint

--- tools/cli/pint.ts
+++ tools/cli/pint.ts
@@ -48,7 +48,7 @@ const pint = command({
     p.executionOptions.outputResult = results;
     PError.showTrace = trace;
 
-    if (!filePath || !filePath.endsWith(".p") || !filePath.includes("/"))
+    if (!filePath || !filePath.endsWith(".p"))
       return p.repl.start(filePath ? [filePath].concat(...args) : args);
 
     p.doFile(filePath, args);
